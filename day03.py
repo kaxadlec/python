fc2065831dffdf42368a2ec419f5c62444bd768a
kaxadlec <kaxadlec@gmail.com>
2023-01-16 14:09:05 +0900

    #4

--- day03.py
+++ day03.py
@@ -1,14 +1,47 @@
-subjects = '  python, data structure, database  $  '
-print(subjects.strip())
-print(subjects.rstrip())
-print(subjects.strip('$'))
+#while문
 
-subjects = '  python, data structure, database  $  '
-print(subjects.find('data'), subjects.index('data'))
-print(subjects.find('ks'))   #- -1 값 리턴
-# print(subjects.index('ksks')) # index는 find와 다르게 exception 던짐
-print(subjects.rfind('data'), subjects.rindex('data')) # reverse find -> 뒤에서부터 찾는다.
-print(subjects.count('data'))  # 'data' 단어는 subjects 문장안에 2개 들어있음.
-print(subjects.title())
+dan = int(input('Dan : '))
+i = 1
+#while 문에서는 \n 없이 줄바꿈 자동으로 됨
+while i <10:
+    # print(f'{dan} * {i} = {dan*i}') # f-string
+    print('{0} * {1} = {2}'.format(dan, i, dan*i))  # format 형태
+    i +=1
+
+if 1<dan<10:
+    i=1
+    while i < 10:
+        print('{0} * {1} = {2}'.format(dan, i, dan*i))  # format 형태
+        i +=1
+else:
+    pass
+
+while True:
+    dan = int(input('Dan :  '))
+
+    if 1< dan < 10:
+        i=1
+        while i <10:
+            print('{0} * {1} = {2}'.format(dan, i, dan*i))
+            i+=1
+        break
+    else:
+        print('2에서 9사이의 값을 입력하세요')
+
+while True:
+    dan = int(input('Dan (0 to quit) :  '))
+
+    if dan == 0:
+        break
+    if 1< dan < 10:
+        i=1
+        while i <10:
+            print('{0} * {1} = {2}'.format(dan, i, dan*i))
+            i+=1
+    else:
+        print('2에서 9사이의 값을 입력하세요')
+
+
+
 
 
