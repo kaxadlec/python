8b8629b0c54f6118a69b5b2480e730150f11669f
kaxadlec <kaxadlec@gmail.com>
2023-01-16 10:20:57 +0900

    #1

---
+++ day03.py
@@ -0,0 +1,28 @@
+a ='abc' \
+   '가나다'
+print(a)
+
+a ='''abc
+가나다'''
+print(a)
+
+# a ='abc
+# 가나다'
+# print(a)   eroor나는 코드
+
+a ='abc\n가나다'
+print(a)
+
+a ='abc\t가나다'
+print(a)
+
+a ="abc\"가나다"
+print(a)
+
+start = 'na ' * 4 +'\n'
+middle = 'hey ' * 3 + '\n'
+end = 'goodbye'
+print(start+start+middle+end)
+
+
+
