ac35b4009774005769475158844505469b0b65c5
kaxadlec <kaxadlec@gmail.com>
2023-01-16 11:05:21 +0900

    #2

--- day03.py
+++ day03.py
@@ -1,28 +1,33 @@
-a ='abc' \
-   '가나다'
-print(a)
+name = "havertz"
+rename = name.replace('h', 'q')
+rename2 = 'x' + name[1:]
+print(name,rename, rename2) #name에 저장된 값 바뀌지 않음
 
-a ='''abc
-가나다'''
-print(a)
+univ = 'Inha University'
+print(univ[5:])
+print(univ[5:15])
+print(univ[0:])  # 0번부터 시작
+print(univ[-1:])    # -1번이 끝자리임. -1번부터 시작
+print(univ[5:-1])  # 5번자리에 있는 문자는 포함되지않고 6번자리부터 시작
+                    # -1번자리에 있는 문자는 포함되지않고 -2번자리 포함.
+print(univ[0:1])
+print(univ[5:6])
+print(univ[-10:-6])
 
-# a ='abc
-# 가나다'
-# print(a)   eroor나는 코드
+print(len(univ))
+print(univ.split()) # split은 문자열을 list함수로 전환 , 띄어쓰기를 구분
+print(univ.split('i')) # i를 기준으로 split
 
-a ='abc\n가나다'
-print(a)
+pokemons_list = ['피카츄', '라이츄', '파이리', '꼬부기']
+pokemons_string = '와 '.join(pokemons_list)   # join함수 활용하여 list 사이에 원하는 문자 넣음
+print(pokemons_list)
+print(pokemons_string)
 
-a ='abc\t가나다'
-print(a)
+setup = 'a duck goes into a sea'
+print(setup.replace('a', 'a famous')) # replace 함수 썼는데 원하지 않는 부분도 바뀜
+print(setup.replace('a ', 'a famous ')) # a 뒤에 띄어쓰기를 통해 원하는 부분만 replace 활용.
 
-a ="abc\"가나다"
-print(a)
 
-start = 'na ' * 4 +'\n'
-middle = 'hey ' * 3 + '\n'
-end = 'goodbye'
-print(start+start+middle+end)
 
 
 
