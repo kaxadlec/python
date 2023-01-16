9b23f6b588e84290538a6d3bc583efa357eb006d
kaxadlec <kaxadlec@gmail.com>
2023-01-16 14:17:28 +0900

    #4-2

--- day03.py
+++ day03.py
@@ -1,32 +1,15 @@
-#while문
-
-dan = int(input('Dan : '))
-i = 1
-#while 문에서는 \n 없이 줄바꿈 자동으로 됨
-while i <10:
-    # print(f'{dan} * {i} = {dan*i}') # f-string
-    print('{0} * {1} = {2}'.format(dan, i, dan*i))  # format 형태
-    i +=1
-
-if 1<dan<10:
-    i=1
-    while i < 10:
-        print('{0} * {1} = {2}'.format(dan, i, dan*i))  # format 형태
-        i +=1
-else:
-    pass
-
-while True:
-    dan = int(input('Dan :  '))
-
-    if 1< dan < 10:
-        i=1
-        while i <10:
-            print('{0} * {1} = {2}'.format(dan, i, dan*i))
-            i+=1
-        break
-    else:
-        print('2에서 9사이의 값을 입력하세요')
+# while True:
+#     dan = int(input('Dan (0 to quit) :  '))
+#
+#     if dan == 0:
+#         break
+#     if 1< dan < 10:
+#         i=1
+#         while i <10:
+#             print('{0} * {1} = {2}'.format(dan, i, dan*i))
+#             i+=1
+#     else:
+#         print('2에서 9사이의 값을 입력하세요')
 
 while True:
     dan = int(input('Dan (0 to quit) :  '))
@@ -34,13 +17,12 @@ while True:
     if dan == 0:
         break
     if 1< dan < 10:
-        i=1
-        while i <10:
+        for i in range(1,10,1):
             print('{0} * {1} = {2}'.format(dan, i, dan*i))
-            i+=1
     else:
         print('2에서 9사이의 값을 입력하세요')
 
+        
 
 
 
