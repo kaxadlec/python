885307e5942b3ed55f486591b80a66f5680d7397
kaxadlec <kaxadlec@gmail.com>
2023-01-17 15:11:40 +0900

    #5 dictionary

--- day04.py
+++ day04.py
@@ -1,67 +1,41 @@
-days = ['Monday', 'Tuesday', 'Wednesday']
-fruits = ['banana', 'orange', 'peach']
-drinks = ['coffee', 'tea', 'beer']
-desserts = ['tiramisu', 'ice cream', 'pie', 'pudding']
-for day, fruit, drink, dessert in zip(days,fruits,drinks,desserts):
+# students = {'name': 'kim inha', 'age': 25, 'eyes':[0.8, 0.7]}
+#
+# # for k in students.key():
+# # for k in students:
+# # for k in students.values():
+# for k, v in students.items():
+#     print(f'{k}:{v}')
+# print(f'이름은 {students["name"]}, 나이는 {students["age"]}세', end = ' ')
+# print(f'시력은 좌: {students["eyes"][0]}, 시력은 우: {students["eyes"][1]}')
 
-    print(day,":drink", drink,"-eat",fruit,"-enjoy",dessert)
-print(list(zip(fruits,drinks)))
-
-number_list = []
-for number in range(1,6):
-    number_list.append(number)
-print(number_list)
-
-number_list = list(range(1,6))
-print(number_list)
-
-number_list = [number for number in range(1,6)]
-print(number_list)
-
-number_list = [number-1 for number in range(1,6)]
-print(number_list)
-
-a_list = [number for number in range(1,6) if number % 3 ==1]
-print(a_list)
-
-a_list = []
-for number in range(1,6):
-    if number % 3 == 1:
-        a_list.append(number)
-print(a_list)
-
-rows = range(1,4)
-cols = range(1,3)
-for row in rows:
-    for col in cols:
-        print(row,col)
-
-rows=range(1,4)
-cols=range(1,3)
-cells = [(row,col) for row in rows for col in cols]
-for cell in cells:
-    print(cell)
-
-for row, col in cells:
-    print(row, col)
-
-number_thing = (number for number in range(1,6))
-print(type(number_thing))
-
-# list comprehension
-
-#옛날방법
-odd_lists = []
-for i in range(1,11):
-    if i % 2 == 1:
-        odd_lists.append(i)
-print(odd_lists)
-
-odd_lists = [i for i in range(1,11) if i % 2 == 1]
-print(odd_lists,type(odd_lists))
-#tuple
-odd_tuples= (i for i in range(1,11) if i % 2 ==1)
-print(odd_tuples,type(odd_tuples))
-
-print(range(1,101))
-print(type(range(1,101)))
+# 안주 추천 프로그램 v0.2
+import random
+alcohol_foods = {
+    '맥주':'치킨',
+    '소주':'골뱅이소면',
+    '와인':'치즈',
+    '고량주':'베이징덕'
+}
+alcohol_list = list(alcohol_foods) # extract keys
+food_list = [food for food in alcohol_foods.values()] # extract values and append list
+#print(alcohol_list, food_list)
+while True:
+    alcohol = input(f'술을 고르시오(메뉴번호만 입력하시오) 1){alcohol_list[0]} 2){alcohol_list[1]} 3){alcohol_list[2]} 4){alcohol_list[3]} 5)랜덤으로 골라요 6)선택안함  ')
+    if alcohol == '6':  #6은 문자열 6이므로 ''써야됨.
+        print('감사합니다.')
+        break
+    elif alcohol == '1':
+        print(f'{alcohol_list[0]}에 어울리는 안주는 {alcohol_foods[alcohol_list[0]]}입니다')
+    elif alcohol == '2':
+        print(f'{alcohol_list[1]}에 어울리는 안주는 {alcohol_foods[alcohol_list[1]]}입니다')
+    elif alcohol == '3':
+        print(f'{alcohol_list[2]}에 어울리는 안주는 {alcohol_foods[alcohol_list[2]]}입니다')
+    elif alcohol == '4':
+        print(f'{alcohol_list[3]}에 어울리는 안주는 {alcohol_foods[alcohol_list[3]]}입니다')
+    elif alcohol == '5':
+        print(f'{random.choice(alcohol_list)}에 어울리는 안주는 {random.choice(food_list)}입니다')
+        #randompick = random.choice(alcohol_list)
+        #print(f'{randompick}에 어울리는 안주는 {alcohol_foods[randompick]}입니다')
+    else:
+        print('알맞은 메뉴번호만 입력해야됩니다!!')
+print(alcohol)
