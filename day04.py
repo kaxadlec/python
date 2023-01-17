5ab99cf41518ae71585528538f27a5b62849488c
kaxadlec <kaxadlec@gmail.com>
2023-01-17 10:58:53 +0900

    #2 tuple

--- day04.py
+++ day04.py
@@ -1,50 +1,27 @@
-# 튜플
-a = 'harry',
-b = ('harry',)
-c = ()  # empty 상태 tuple
-d = 'harry', 'ron'  # packing
-e = ('hermione')  # 이거는 그냥 string임!
-f = ('harry','ron')  # packing
-g = ('hermione',)
-print(f+g)  # 튜플은 결합가능
-print(f[1])
-x,y = f  # unpacking
-print(y)  # print(f[1])과 같음
-# print(type(a))
-# print(type(b))
-# print(type(c))
-# print(type(d))
-# print(type(e))
-# print(type(f))
-h=f+g
-print(h)
-h +=g  # 튜플 결합하여 수정된 것처럼 보임
-print(h)
-
-print(g, id(g))
-g+=f
-print(g,id(g))  #튜플 결합하면 id 달라짐
-
-# list
-scores = ('B+', 'A+', 'C+')
-print(scores)
-print(scores[1], scores[2])
-temp = list(scores)
-print(temp)
-temp[1] = 'C+'
-temp[2] = 'A+'
-print(temp)
-scores = tuple(temp)
-print(scores[1],scores[2])
-
-marxes = ['Groucho', 'Chico', 'Harpo']
-print(marxes[0:2])
-print(marxes[::2])
-print(marxes[::])
-print(marxes[3:])
-print(marxes[2:])
-marxes.append('Zeppo')
-print(marxes)
+chelsea = ['Havertz','Mount','James','Sliva','Kante']
+mancity = ['Haaland', 'De Bruyne']
+arsenal = ['Saka', 'Saliba']
+chelsea.insert(1,'Mudryk')
+#print(chelsea*2)
+mancity.extend(chelsea)  # mancity = mancity + chelsea 와 같음.
+arsenal.append(chelsea)
+print(mancity)
+print(arsenal)
+print(arsenal[2][0])  # Havertz!
+print(arsenal[-1][-6])  # Havertz!
+arsenal[-2] = 'Party'
+print(arsenal)
+# print(arsenal.pop()) # chelsea pop
+# print(arsenal) # chelsea 빠짐
+print(arsenal[2].pop())  # 맨 뒤 kante pop
+print(arsenal)  # kante 빠짐
+print(arsenal[2].pop(1)) # mudryk pop
+print(arsenal)
+del arsenal[2][-2]
+print(arsenal) # James delete(pop)
+# arsenal.remove('Mount') # not in arsenal
+arsenal[2].remove('Mount')
+print(arsenal)
 
 
 
