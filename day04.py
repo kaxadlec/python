6e062b994daeb81faf288c4cb6d279c27f5a8e6e
kaxadlec <kaxadlec@gmail.com>
2023-01-17 13:43:56 +0900

    #4 (modified)

--- day04.py
+++ day04.py
@@ -3,7 +3,8 @@ fruits = ['banana', 'orange', 'peach']
 drinks = ['coffee', 'tea', 'beer']
 desserts = ['tiramisu', 'ice cream', 'pie', 'pudding']
 for day, fruit, drink, dessert in zip(days,fruits,drinks,desserts):
-print(day,":drink", drink,"-eat",fruit,"-enjoy",dessert)
+
+    print(day,":drink", drink,"-eat",fruit,"-enjoy",dessert)
 print(list(zip(fruits,drinks)))
 
 number_list = []
