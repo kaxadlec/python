e1b82dd210074282a423c7e65df826230a7f7ee3
kaxadlec <kaxadlec@gmail.com>
2023-01-18 14:09:01 +0900

    #4 fee using dict

--- day05.py
+++ day05.py
@@ -1,17 +1,10 @@
-#v0.2
-
+#v0.2 dictionary
 import random
-
-# person = input("인원을 입력하시오.")
-# for i in person:
-#     ages = random.randint(1,100)
-
-#def calculate_fee(*args):
-def calculate_fee(args)->list:  # type hint
+def calculate_fee(args)->dict:  # dictionary 이용
     """
     놀이공원 요금 계산 프로그램
     :param args: ages in list
-    :return: 전체 인원 수, 어른 수, 아이 수, 지불할 총 입장료
+    :return: {'no_of_people':전체 인원 수, 'no_of_adult' : 어른 수, 'no_of_kid' : 아이 수, 'total fee' :지불할 총 입장료}
     """
     total = 0   #돈 초기화
     adults=0  # 인원수 초기화
@@ -23,10 +16,10 @@ def calculate_fee(args)->list:  # type hint
         else:
             total = total +3000
             kids+=1
-    return [len(args), adults, kids, total]
+    return{'no_of_people' : len(args), 'no_of_adult' : adults, 'no_of_kid' : kids, 'total_fee' :total}
 
 num_of_visitor = int(input('몇 명입니까? '))
-ages = [random.randint(1,60) for i in range(num_of_visitor)]  # list comprehension 시작
+ages= {random.randint(1,60) for i in range(num_of_visitor)}  # dictionary comprehension 시작
 print(ages)
 result = calculate_fee(ages)
-print(f'총 {result[0]}명 중에 어른 {result[1]}명, 아이  {result[2]}명 방문하셨습니다.\n총 요금은 {result[3]}원입니다.')
+print(f"총 {result['no_of_people']}명 중에 어른 {result['no_of_adult']}명, 아이  {result['no_of_kid']}명 방문하셨습니다.\n총 요금은 {result['total_fee']}원입니다.")
