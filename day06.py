9e110d2a830266631471093690738bd0ac06ecd9
kaxadlec <kaxadlec@gmail.com>
2023-01-19 15:31:49 +0900

    class

--- day06.py
+++ day06.py
@@ -1,36 +1,45 @@
-## except 실습
-try:
-    # raise Exception("뭐함")
-    raise TypeError('타입에러~~~')
-    expr = input('분자 분모 입력 (두 수는 띄어쓰기를 통해 구분하삼): ').split()
-    print(int(expr[0])/ int(expr[1]))
-    # print(expr[2]) # 에러 나와서 except 프린트 됨.
-#ValueError
-#ZeroDivisionError
-except ValueError as err:
-    print(f'숫자를 입력해라 좀 ({err})')
-except ZeroDivisionError as err:
-    print(err)
-    print('분모에 0을 넣는 바보같은 짓을 하셨습니다.')
-except IndexError:
-    print('입력 값의 범위에 문제가 있습니다')
-except Exception as other:
-    print(f'최후의 exception: {other}')
-#except:
-#     print('봵뷃뷁')
-else:  # 예외가 발생하지 않았을 때
-    print('프로그램 정상적으로', end=' ')
-finally:    # 예외 발생 여부와 상관없이 무조건 실행
-    print('종료')
+# class
+# class Pokemon:
+#     def __init__(self):
+#         print("포켓몬 객체 생성된다")
+# p1 = Pokemon()  # 포켓몬 객체 생성된다
+# p2 = Pokemon()  # 포켓몬 객체 생성된다
+# print(p1,p2)  # <__main__.Pokemon object at 0x000002497832C6D0> <__main__.Pokemon object at 0x000002497832C750>
 
-# def div_calc(a,b):
-#     """
-#     나누기 함수
-#     :param a: 분자
-#     :param b: 분모
-#     :return: 계산결과
-#     """
-#     return a/b
+# class Pokemon:
+#     def __init__(self, name):
+#         self.name = name
+#         print(f'{name} 생성')
+#
+# p1 = Pokemon('파이리')
+# p2 = Pokemon('고라파덕')
+# print(p1.name,p2.name)
 
-# print(div_calc(18,6))
-# print(div_calc(18,0)) # ZeroDivisionError: division by zero
+# class Pokemon:
+#     def __init__(self, name, owner):
+#         self.name = name
+#         self.owner = owner
+#         print(f'{name} 생성')
+#
+# p1 = Pokemon('파이리', '빛나')
+# p2 = Pokemon('고라파덕', '지우')
+# print(f'{p1.owner}는 {p1.name}를 선택했다.')
+# print(f'{p2.owner}는 {p2.name}를 선택했다.')
+
+class Pokemon:
+    def __init__(self, name, owner,skills):
+        self.name = name
+        self.owner = owner
+        self.skills = skills.split('/')
+        print(f'{name} 생성')
+
+    def info(self):
+        print(f'{self.owner}의 포켓몬은 {self.name}입니다.')
+        for skill in self.skills:
+            print(skill)
+
+#p1 = Pokemon('파이리', '빛나', '불꽃세례/회오리불꽃/화염방사')
+p2 = Pokemon('고라파덕','지우','할퀴기/꼬리흔들기/물대포')
+# p1.info()
+p2.info()
+# print(p1.skills)
